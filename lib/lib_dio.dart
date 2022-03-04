8f5abab26b403e0a04e213460d356672efcd803a
xj <xj62636150@gmail.com>
2022-03-04 12:44:47 +0800

    公开插件库的包

--- lib/lib_dio.dart
+++ lib/lib_dio.dart
@@ -10,3 +10,7 @@ export 'com/flutter/http/utils/MD5Utils.dart';
 export 'com/flutter/http/RxDio.dart';
 export 'com/flutter/http/RxDioConfig.dart';
 export 'com/flutter/http/adapter/ResponseDatas.dart';
+
+export 'package:dio/dio.dart';
+export 'package:json_annotation/json_annotation.dart';
+export 'package:rxdart/rxdart.dart';
