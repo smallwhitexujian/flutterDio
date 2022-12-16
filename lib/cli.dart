edca591feb4939cdfb418db0f1b48ef65d4bcfa3
xj626361950 <xj626361950@163.com>
2022-12-16 17:50:41 +0800

    1.2.2

--- lib/cli.dart
+++ lib/cli.dart
@@ -1,16 +1,18 @@
-import 'package:shelf/shelf_io.dart' as shelf_io;
-import 'package:shelf_proxy/shelf_proxy.dart';
+// import 'package:shelf/shelf_io.dart' as shelf_io;
+// import 'package:shelf_proxy/shelf_proxy.dart';
 
-void main(List<String> arguments) async {
-  /// 绑定本地端口localhost，61815，转发到真正的服务器中
-  final server = await shelf_io.serve(
-    //设置代理的目标host
-    proxyHandler('https://wanandroid.com/'),
-    'localhost',//代理的地址
-    61815,//代理的端口
-  );
-  //设置请求头允许跨域
-  server.defaultResponseHeaders.add('Access-Control-Allow-Origin', '*');
-  server.defaultResponseHeaders.add('Access-Control-Allow-Credentials', true);
-  print('Proxying at http://${server.address.host}:${server.port}');
-}
+// shelf_proxy: ^1.0.2 导报
+//
+// void main(List<String> arguments) async {
+//   /// 绑定本地端口localhost，61815，转发到真正的服务器中
+//   final server = await shelf_io.serve(
+//     //设置代理的目标host
+//     proxyHandler('https://wanandroid.com/'),
+//     'localhost',//代理的地址
+//     61815,//代理的端口
+//   );
+//   //设置请求头允许跨域
+//   server.defaultResponseHeaders.add('Access-Control-Allow-Origin', '*');
+//   server.defaultResponseHeaders.add('Access-Control-Allow-Credentials', true);
+//   print('Proxying at http://${server.address.host}:${server.port}');
+// }
